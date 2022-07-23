ef6a72a1ecf58d69f35733b55c90d0fd9decfc97
Shaik Shabber <87518304+Shabber1412@users.noreply.github.com>
2022-07-23 19:09:04 +0530

    Update Ec2instance.tf

--- Ec2instance.tf
+++ Ec2instance.tf
@@ -1,7 +1,6 @@
 resource "aws_instance" "myec2" {
     ami = var.ami
     instance_type = var.instance_type
-    //private_ip = var.private_subnet
     subnet_id = aws_subnet.private.id
     tags = {
       Name = "Terraform Ec2_Instance-1"
@@ -12,7 +11,6 @@ resource "aws_instance" "myec2" {
 resource "aws_instance" "myec2_instance" {
     ami = var.ami
     instance_type = var.instance_type
-    //private_ip = var.private_subnet
     subnet_id = aws_subnet.public.id
     tags = {
       Name = "Terraform Ec2_Instance-2"
